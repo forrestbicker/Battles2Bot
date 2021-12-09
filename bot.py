163bf90956bdcc3a0f7d242ea19d09c489b2f85e
Forrest Bicker <bickerforrest@gmail.com>
2021-12-08 22:40:04 -0800

    continually check if you or your opponent disconnected, also only attempt to randomly place towers on your side

--- bot.py
+++ bot.py
@@ -12,22 +12,37 @@ def click(imgfile, max_tries=30):
         if res is None:
             print(f"Button {imgfile} not found on attempt {attempts}/{max_tries}")
             attempts += 1
-            continue
-        break
+        else:
+            break
+        if attempts % 10 == 0:
+            # if opponent disconnected, catch it here and re-enter lobby
+            exiter = pyautogui.locateCenterOnScreen('assets/exit.png', confidence=0.7)
+            if exiter is not None:
+                pyautogui.click(exiter.x, exiter.y)
+                return 'restart'
+            # if we disconnected, catch it here and re-enter lobby
+            exiter = pyautogui.locateCenterOnScreen('assets/disconnect.png', confidence=0.7)
+            if exiter is not None:
+                pyautogui.click(exiter.x, exiter.y)
+                return 'restart'  # resturn restart if we disconnected
     if res is not None:
         for i in range(3):
             pyautogui.click(res.x, res.y)
             time.sleep(1)
-        return True
-    return False
+        return (res.x, res.y)  # return the xy coordinates of the button if we click
+    return None  # return None if we didn't click anything
 
-def place_tower(key):
+def place_tower(key, player):
     win_corner = pyautogui.locateCenterOnScreen("assets/window_title.png", confidence=0.8)
-    xmin, ymin = win_corner.x + 100, win_corner.y + 100
-    xmax, ymax = win_corner.x + 1450, win_corner.y + 940
+    if player == 0:
+        xmin, ymin = win_corner.x + 150, win_corner.y + 100
+        xmax, ymax = win_corner.x + 800, win_corner.y + 940
+    else:
+        xmin, ymin = win_corner.x + 1000, win_corner.y + 100
+        xmax, ymax = win_corner.x + 1450, win_corner.y + 940
     for i in range(20):
-        res = pyautogui.locateCenterOnScreen("assets/round.png", confidence=0.7)
-        if res is None:
+        res = pyautogui.locateCenterOnScreen("assets/ok.png", confidence=0.7)
+        if res is not None:
             return False
         pydirectinput.press(key)
         x, y = random.randrange(xmin, xmax), random.randrange(ymin, ymax)
@@ -38,26 +53,31 @@ def place_tower(key):
 
 while True:
     # queue into battle
-    click("assets/battle.png")
-    click("assets/ready.png")
-    click("assets/battle2.png")
-    if click("assets/disconnect.png", max_tries=3):  # if opponent disconnected, catch it here and re-enter lobby
+    if click("assets/battle.png") == 'restart':
+        continue
+    if click("assets/ready.png") == 'restart':
+        continue
+    if click("assets/battle2.png") == 'restart':
         continue
 
-    time.sleep(6) # wait for battle to start
+    time.sleep(6)  # wait for battle to start
     # once in battle, send bloons and place towers
-    click("assets/redbloon.png")  # wait for round to start
-    [pydirectinput.press('a') for i in range(5)]  # send red bloon rush
-    [pydirectinput.press('s') for i in range(2)]  # layer blue bloons
+    coords = click("assets/redbloon.png")  # wait for round to start
+    if coords is not None and len(coords) == 2:
+        x, y = coords
+        player = 0 if x < 500 else 1
+        [pydirectinput.press('a') for i in range(5)]  # send red bloon rush
+        [pydirectinput.press('s') for i in range(2)]  # layer blue bloons
 
-    shouldcontinue = place_tower("q")  # randomly place hero
-    if shouldcontinue:  # if the game ended already, stop placing towers
-        place_tower("w")  # randomly place 1st tower
-    # if shouldcontinue:
-    #     time.sleep(28)
-    #     click("assets/surrender.png", max_tries=4)
-    #     time.sleep(2)
-    #     click("assets/confirm.png", max_tries=4)
-    click("assets/ok.png", max_tries=64)  # wait until you leak to death and the win screen pops up. this may take a while
+        shouldcontinue = place_tower("q", player)  # randomly place hero
+        if shouldcontinue is not None:  # if the game ended already, stop placing towers
+            place_tower("w", player)  # randomly place 1st tower
+        # if shouldcontinue:
+        #     time.sleep(28)
+        #     click("assets/surrender.png", max_tries=4)
+        #     time.sleep(2)
+        #     click("assets/confirm.png", max_tries=4)
+    if click("assets/ok.png", max_tries=128) == 'restart':  # wait until you leak to death and the win screen pops up. this may take a while
+        continue
     click("assets/discard.png", max_tries=3)  # in case we recieve a chest, discard it
     click("assets/back.png", max_tries=3)  # back to main menu
